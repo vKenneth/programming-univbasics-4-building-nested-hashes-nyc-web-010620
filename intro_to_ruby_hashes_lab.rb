d1412fc0e8448997d5bed7fe728b7badce52a205
K e n V <kenneth_valdivia@yahoo.com>
2019-12-26 05:24:04 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -2,7 +2,7 @@ def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
 	{}
-end
+end 
 
 def monopoly_with_second_tier
   # When you start writing the implementation for this method, copy the Hash
