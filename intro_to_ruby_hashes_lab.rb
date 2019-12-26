2ffa0ef64682e2483be346ba2a8127431aff3102
K e n V <kenneth_valdivia@yahoo.com>
2019-12-26 05:26:18 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,4 +29,5 @@ end
 
  expect(monopoly_with_fourth_tier[:railroads][:names][:b_and_o_railroad]["mortgage_value"]).to eq("$400")
      end		     
-     
+     it "sets the 1st key of the :shortline_railroad hash to a string, 'mortgage_value', whose value is also a string, '$800'" do
+       
